321fea073297cace0578102e1c088952286b700c
Nino Richter <nino.richter@tum.de>
2024-10-24 14:00:29 +0200

    Reranking with Cohere on Azure

--- app/utils/dependencies.py
+++ app/utils/dependencies.py
@@ -5,9 +5,9 @@ from app.models.model_loader import get_model
 from app.prompt.prompt_manager import PromptManager
 from app.utils.environment import config
 
-# Initialize resources
+# Initialize resources  
 model = get_model()
-reranker = Reranker(model=model, api_key=config.COHERE_API_KEY)
+reranker = Reranker(model=model, api_key_en=config.COHERE_API_KEY_EN, api_key_multi=config.COHERE_API_KEY_MULTI)
 weaviate_manager = WeaviateManager(config.WEAVIATE_URL, embedding_model=model, reranker=reranker)
 prompt_manager = PromptManager()
 request_handler = RequestHandler(weaviate_manager=weaviate_manager, model=model, prompt_manager=prompt_manager)
