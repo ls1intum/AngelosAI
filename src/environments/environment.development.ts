b24aeaf29befb65ed4f83e935f3ede1bfbd521ab
nickschneider <n.schneider@tum.de>
2025-02-07 15:56:41 +0100

    adjust compose

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    angelosUrl: '/api/chat',
+    angelosUrl: 'http://localhost:9007/api/chat',
     angelosAppApiKey: 'SOME_SECRET_KEY',
-    organisation: 2,
+    organisation: 102,
     filterByOrg: true,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: true,
     angelosUrl: 'http://localhost:9007/api/chat',
     angelosAppApiKey: 'SOME_SECRET_KEY',
-    organisation : 2,
+    organisation: 102,
     filterByOrg: true,
 };
