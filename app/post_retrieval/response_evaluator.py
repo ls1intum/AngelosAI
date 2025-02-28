3f81cbc8b306a32ac31cbcaaa848f73a5db05610
nickschneider <n.schneider@tum.de>
2025-02-28 12:56:38 +0100

    logging if llm as a judge says answer is not valid

--- app/post_retrieval/response_evaluator.py
+++ app/post_retrieval/response_evaluator.py
@@ -3,11 +3,12 @@ import logging
 from app.models.base_model import BaseModelClient
 from app.prompt.prompt_manager import PromptManager
 
+
 class ResponseEvaluator:
     def __init__(self, model: BaseModelClient, prompt_manager: PromptManager):
         self.model = model
         self.prompt_manager = prompt_manager
-        
+
     def process_response(self, question: str, response: str, language: str) -> str:
         if response != "False":
             response_valid: bool = self.evaluate_response(question=question, response=response, language=language)
@@ -19,12 +20,14 @@ class ResponseEvaluator:
             else:
                 response = "False"
         return response
-        
+
     def evaluate_response(self, question: str, response: str, language: str) -> bool:
-        prompt = self.prompt_manager.create_response_evaluation_messages(question=question, answer=response, language=language)   
+        prompt = self.prompt_manager.create_response_evaluation_messages(question=question, answer=response,
+                                                                         language=language)
         output = self.model.complete(prompt).strip()
-                
+
         if "OK" in output:
             return True
         else:
+            logging.error("LLM as a judge does not think the answer is appropriate.")
             return False
