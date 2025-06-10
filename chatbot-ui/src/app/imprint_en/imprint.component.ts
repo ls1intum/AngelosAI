c7b18d84fb07e0ccda79a6d455e161b7580e3d7f
Nino Richter <nino.richter@tum.de>
2025-06-10 22:47:52 +0200

    Add english imprint and privacy statement

---
+++ chatbot-ui/src/app/imprint_en/imprint.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../utils/header/header.component';
+
+@Component({
+  selector: 'app-imprint',
+  standalone: true,
+  imports: [
+    HeaderComponent
+  ],
+  templateUrl: './imprint.component.html',
+  styleUrl: './imprint.component.scss'
+})
+export class ImprintEnComponent {
+
+}
---
+++ chatbot-ui/src/app/privacy_en/privacy.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../utils/header/header.component';
+
+@Component({
+  selector: 'app-privacy',
+  standalone: true,
+  imports: [
+    HeaderComponent
+  ],
+  templateUrl: './privacy.component.html',
+  styleUrl: './privacy.component.scss'
+})
+export class PrivacyEnComponent {
+
+}
